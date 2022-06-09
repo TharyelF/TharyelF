49470e67707066268b77caa96bc3b8fb6d475b0d
Tharyel Faria <temptharyel@gmail.com>
2022-06-09 12:08:06 -0300

    atualizado

---
+++ Projeto.py
@@ -0,0 +1,18 @@
+# O Inicio
+
+print('Salve cambada de newba!')
+print('Salve')
+print('Salvados')
+print("Python é a melhor linguagem") 
+
+# Matematica Básica
+
+print(1000+10)
+print((2*7)+3)
+print(10*(8-2))
+print(4/(12+6))
+print(68*2)
+print((4+8)/2)
+
+print (3/4)
+print(0.42)
