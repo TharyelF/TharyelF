77de2b11f63b1bfe957ae288a5ed1bb7ba32d9d2
Tharyel Faria <temptharyel@gmail.com>
2022-06-13 14:58:36 -0300

    main

--- Number.py
+++ Number.py
@@ -4,13 +4,32 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
+        guess = int(input(f'Advinhe um número entre 1 e {x}: '))
         if guess < random_number:
-            print('Sorry, guess again. Too low.')
+            print('Desculpe, tente novamente. Muito baixo.')
         elif guess > random_number:
-            print('Sorry, guess again. Too High.') 
+            print('Desculpe, tente novamente. Muito alto.') 
 
-    print(f'Yay, congrats. You have guessed the number {random_number} Correctly!!!')
+    print(f'Isso!!, Parabens. Voçe Adivinhou o Número {random_number} !!!')
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+             guess = random.randint(low, high) 
+        else:
+             guess = low # Tambem pod ser  high b/c low = high /        
+        feedback = input(f'Is {guess} Muito alto (H), Muito baixo (L) or Correto (C)?? ').lower()
+        if feedback =='H':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess +1
+
+    print(f'Isso!! O computador acertou o número {guess}')        
+
+
+computer_guess(50)
 
 guess(50)
